ec046ed2fa95444a39cf4a858a704ea1a0d63d5e
DaRoastFTW <daroastftw@gmail.com>
2021-09-01 14:31:36 -0700

    Added Top_Level document.
    By DMC

---
+++ labs1-3/labs1-3.srcs/sources_1/new/Top_Level.v
@@ -0,0 +1,35 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/01/2021 02:18:12 PM
+// Design Name: 
+// Module Name: Top_Level
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Top_Level(Clk, Rst, Instruction, out7, en_out);
+    input Clk, Rst;
+    output [31:0] Instruction;
+    
+    wire ClkOut;
+    output [6:0] out7;
+    output [7:0] en_out;
+    
+    
+    ClkDiv(Clk, Rst, ClkOut);
+    InstructionFetchUnit(Instruction, Rst, ClkOut);
+    Two4DigitDisplay(Clk, Instruction[15:0], Instruction[31:16], out7, en_out);
+endmodule
