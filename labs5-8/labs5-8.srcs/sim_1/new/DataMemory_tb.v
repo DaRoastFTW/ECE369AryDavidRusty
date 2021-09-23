1efdc4812bb66761233ddade90cbf0b87bbe27f7
RinehartRacing <rustyrinehart@email.arizona.edu>
2021-09-23 12:49:11 -0700

    Completed DataMemory code and testbench

---
+++ labs5-8/labs5-8.srcs/sim_1/new/DataMemory_tb.v
@@ -0,0 +1,59 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// ECE369 - Computer Architecture
+// 
+// Module - DataMemory_tb.v
+// Description - Test the 'DataMemory.v' module.
+////////////////////////////////////////////////////////////////////////////////
+
+module DataMemory_tb(); 
+
+    reg     [31:0]  Address;
+    reg     [31:0]  WriteData;
+    reg             Clk;
+    reg             MemWrite;
+    reg             MemRead;
+
+    wire [31:0] ReadData;
+
+    DataMemory u0(
+        .Address(Address), 
+        .WriteData(WriteData), 
+        .Clk(Clk), 
+        .MemWrite(MemWrite), 
+        .MemRead(MemRead), 
+        .ReadData(ReadData)
+    ); 
+
+	initial begin
+		Clk <= 1'b0;
+		forever #10 Clk <= ~Clk;
+	end
+
+	initial begin
+	
+    /* Please fill in the implementation here... */
+    //Test Writing first
+    Address <= 0;
+    WriteData <= 858;
+    MemWrite <= 0;
+    @(posedge Clk);
+    @(posedge Clk);
+    MemWrite <= 1;
+    @(posedge Clk);
+    @(posedge Clk);
+    MemRead <= 1;
+    @(posedge Clk);
+	MemWrite <= 0;
+	MemRead <= 0;
+	Address <= 8;
+	@(posedge Clk);
+	WriteData <= 67890;
+	@(posedge Clk);
+	MemWrite <= 1;
+	
+	end
+
+endmodule
+
