8f5e802a45be8900426d0375bb27a1ff81241012
RinehartRacing <rustyrinehart@email.arizona.edu>
2021-10-07 09:11:59 -0700

    Successfully Read Instructions from Instructions.txt into Controller testbench

--- labs5-8/labs5-8.srcs/sim_1/new/Controller_tb.v
+++ labs5-8/labs5-8.srcs/sim_1/new/Controller_tb.v
@@ -26,7 +26,7 @@ wire [31:0] Instruction;
 //reg [31:0] memory [0:231];
 
 integer i;
-reg [7:0] memory [0:15]; // 8 bit memory with 16 entries
+reg [31:0] memory [0:504]; // 8 bit memory with 16 entries
 
 
 initial
