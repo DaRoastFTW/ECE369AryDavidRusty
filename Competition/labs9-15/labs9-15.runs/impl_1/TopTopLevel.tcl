910464d97e5a8182df1361e2fb2372135f1e5777
RinehartRacing <rustyrinehart@email.arizona.edu>
2021-12-04 15:58:36 -0700

    Better ClkDiv

--- Competition/labs9-15/labs9-15.runs/impl_1/TopTopLevel.tcl
+++ Competition/labs9-15/labs9-15.runs/impl_1/TopTopLevel.tcl
@@ -60,15 +60,13 @@ proc step_failed { step } {
   close $ch
 }
 
-set_msg_config -id {Synth 8-256} -limit 10000
-set_msg_config -id {Synth 8-638} -limit 10000
 
 start_step init_design
 set ACTIVE_STEP init_design
 set rc [catch {
   create_msg_db init_design.pb
   set_param chipscope.maxJobs 4
-  set_param synth.incrementalSynthesisCache C:/Users/rrine/AppData/Roaming/Xilinx/Vivado/.Xil/Vivado-50508-DELL-TACO-2/incrSyn
+  set_param xicom.use_bs_reader 1
   create_project -in_memory -part xc7a100tcsg324-1
   set_property design_mode GateLvl [current_fileset]
   set_param project.singleFileAddWarning.threshold 0
--- Competition/labs9-15/labs9-15.srcs/sources_1/new/ClkDiv.v
+++ Competition/labs9-15/labs9-15.srcs/sources_1/new/ClkDiv.v
@@ -8,7 +8,7 @@ module ClkDiv (
   input Clk, Rst;
   output reg ClkOut;
   //to create 1 Hz clock from 100-MHz on the board
-  parameter DivVal = 100;
+  parameter DivVal = 10000;
   reg [25:0] DivCnt;
   reg ClkInt;
 
